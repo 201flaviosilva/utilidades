f9aec85b94d49bb0789234a55d91ada61da9883d
201flaviosilva <flavioandre.2001.9.7@gmail.com>
2022-05-10 17:07:29 +0000

    add some configs

--- src/index.js
+++ src/index.js
@@ -132,6 +132,19 @@ export const deleteAllChildDom = (domElement) => {
 	}
 }
 
+/**
+ * Compare if two object are equal
+ * 
+ * @example
+ * compare2Objects({a:1, b:2}, {a: 1}); // false
+ * compare2Objects({a:1, b:2}, {a: 1, b: 2 }); // true
+ * 
+ * @param {*} object1 - the first object/array to compare
+ * @param {*} object2 - the second object/array to compare
+ * @returns {boolean} - true if the two object are equal
+ */
+export const compare2Objects = (object1, object2) => JSON.stringify(object1) === JSON.stringify(object2);
+
 // Export a file
 export const download = (data, filename, type) => {
 	const file = new Blob([data], { type: type });
---
+++ types/index.d.ts
@@ -0,0 +1,58 @@
+export function randomFloat(min: any, max: any, precision?: number): number;
+export function randomInt(min: any, max: any): number;
+export function randomColor(): string;
+export function randomColor0X(): string;
+export function randomRGBColor(): string;
+export function randomRGBAColor(): string;
+export function radiansToDegrees(r: any): number;
+export function degreesToRadians(d: any): number;
+export function sortAscending(arr: any): any;
+export function sortDescending(arr: any): any;
+export function formateScore(time: any): number;
+export function map(value: any, x1: any, y1: any, x2: any, y2: any): any;
+export function distanceTwoPoints(x1: any, y1: any, x2: any, y2: any): number;
+export function distanceTwoPointsVector2(point1: any, point2: any): number;
+export function angleBetweenTwoPoints(x1: any, y1: any, x2: any, y2: any): number;
+export function angleBetweenTwoPointsVector2(point1: any, point2: any): number;
+export function angleBetweenTwoPointsDegrees(x1: any, y1: any, x2: any, y2: any): number;
+export function angleBetweenTwoPointsVector2Degrees(point1: any, point2: any): number;
+export function distanceBetweenTwoCirclesFromCircle(circle1: any, circle2: any): number;
+export function distanceBetweenTwoCircles(x1: any, y1: any, radius1: any, x2: any, y2: any, radius2: any): number;
+export function circleArea(radius: any): number;
+export function circlePerimeter(radius: any): number;
+export function distanceBetweenTwoRectanglesFromBounds(rect1: any, rect2: any): number;
+export function distanceBetweenTwoRectangles(x1: any, y1: any, width1: any, height1: any, x2: any, y2: any, width2: any, height2: any): number;
+export function angleBetweenTwoRectanglesFromBounds(rect1: any, rect2: any): number;
+export function angleBetweenTwoRectangles(x1: any, y1: any, width1: any, height1: any, x2: any, y2: any, width2: any, height2: any): number;
+export function rectangleArea(width: any, height: any): number;
+export function rectanglePerimeter(width: any, height: any): number;
+export function rectanglePerimeterFromBounds(rectangle: any): number;
+export function rectangleVertices(x: any, y: any, width: any, height: any): {
+    x: any;
+    y: any;
+}[];
+export function rectangleVerticesFromBounds(rectangle: any): {
+    x: any;
+    y: any;
+}[];
+export function rectangleCenterX(x: any, width: any): any;
+export function rectangleCenterY(y: any, height: any): any;
+export function rectangleCenter(x: any, y: any, width: any, height: any): {
+    x: any;
+    y: any;
+};
+export function rectangleCenterXFromBounds(rectangle: any): any;
+export function rectangleCenterYFromBounds(rectangle: any): any;
+export function rectangleCenterFromBounds(rectangle: any): {
+    x: any;
+    y: any;
+};
+export function allCharactersSame(string: any): boolean;
+export function sortAscendingObj(arr: any, prop: any): any;
+export function sortDescendingObj(arr: any, prop: any): any;
+export function getMinArrayObjects(arr: any, prop: any): any;
+export function getMaxArrayObjects(arr: any, prop: any): any;
+export function getDateFormatted(): string;
+export function deleteAllChildDom(domElement: any): void;
+export function compare2Objects(object1: any, object2: any): boolean;
+export function download(data: any, filename: any, type: any): void;
