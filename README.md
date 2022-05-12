734ac567325092334ed7b989b89324fe27deefa8
201flaviosilva <flavioandre.2001.9.7@gmail.com>
2022-05-12 14:15:40 +0000

    Add printObjectInDOM and getUrlParameter

--- README.md
+++ README.md
@@ -3,6 +3,18 @@
 ## Description
 Just some simple JavaScript Utils
 
+## Usage
+
+### Create a `.npmrc`
+```sh 
+echo @201flaviosilva:registry=https://gitlab.com/api/v4/projects/35983470/packages/npm/ >> .npmrc
+```
+
+### Install Package
+```sh
+npm i @201flaviosilva/utils
+```
+
 ## Links
 - [WebPage](https://201flaviosilva.gitlab.io/utilsjs/);
 - [Package](https://gitlab.com/201flaviosilva/utilsjs/-/packages);
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,16 @@
+/**
+ * Return a random float number between the given values and the given precision
+ *
+ * @example
+ * randomFloat(0, 1);
+ * randomFloat(-10, 0, 5);
+ * randomFloat(-550, 444);
+ *
+ * @param {number} min - min value
+ * @param {number} max - max value
+ * @param {number} precision - the float precision
+ * @returns {number} - random number
+ */
 export function randomFloat(min: number, max: number, precision?: number): number;
 export function randomInt(min: any, max: any): number;
 export function randomColor(): string;
@@ -9,6 +22,18 @@ export function degreesToRadians(d: any): number;
 export function sortAscending(arr: any): any;
 export function sortDescending(arr: any): any;
 export function formateScore(time: any): number;
+/**
+ * Re-maps a number from one range to another
+ *
+ * @see {@link https://gist.github.com/xposedbones/75ebaef3c10060a3ee3b246166caab56#gistcomment-2951694}
+ *
+ * @param {*} value
+ * @param {*} x1
+ * @param {*} y1
+ * @param {*} x2
+ * @param {*} y2
+ * @returns {*} - value
+ */
 export function map(value: any, x1: any, y1: any, x2: any, y2: any): any;
 export function distanceTwoPoints(x1: any, y1: any, x2: any, y2: any): number;
 export function distanceTwoPointsVector2(point1: any, point2: any): number;
@@ -47,6 +72,9 @@ export function rectangleCenterFromBounds(rectangle: any): {
     x: any;
     y: any;
 };
+/**
+ * Check if all characters are equal in a string
+ */
 export function allCharactersSame(string: any): boolean;
 export function sortAscendingObj(arr: any, prop: any): any;
 export function sortDescendingObj(arr: any, prop: any): any;
@@ -54,5 +82,51 @@ export function getMinArrayObjects(arr: any, prop: any): any;
 export function getMaxArrayObjects(arr: any, prop: any): any;
 export function getDateFormatted(): string;
 export function deleteAllChildDom(domElement: any): void;
+/**
+ * Compare if two object are equal
+ *
+ * @example
+ * compare2Objects({a:1, b:2}, {a: 1}); // false
+ * compare2Objects({a:1, b:2}, {a: 1, b: 2 }); // true
+ *
+ * @param {*} object1 - the first object/array to compare
+ * @param {*} object2 - the second object/array to compare
+ * @returns {boolean} - true if the two object are equal
+ */
 export function compare2Objects(object1: any, object2: any): boolean;
 export function download(data: any, filename: any, type: any): void;
+/**
+ * Get a value from a query parameter from the current URL
+ *
+ * @example
+ * URL: http://localhost:8080/?name=Silva
+ * getUrlParameter("name"); // Silva
+ *
+ * @param {string} key
+ * @returns {string} - Returns the value of the key
+ */
+export function getUrlParameter(key: string): string;
+/**
+ * Print JavaScript Object In HTML Dom
+ *
+ * @example
+ * const myObject = {
+ * 	name: "Sistema Solar",
+ * 	numberStarts: 1,
+ * 	terra: {
+ * 		speed: 29.783,
+ * 		moons: ["Lua"],
+ * 		temperature: {
+ * 			min: 93.2,
+ * 			max: 57.8,
+ * 			average: 14,
+ * 		}
+ * 	},
+ * };
+ * print(myObject, document.getElementById("myDomElement"));
+ *
+ * @param {*} object - JavaScript Object to print in the dom
+ * @param {*?} [parent=document.body] - DOM element to print
+ * @see {@link -  https://jsfiddle.net/201flaviosilva/mbnz3p7y/}
+ */
+export function printObjectInDOM(object: any, parent?: any | null): void;
