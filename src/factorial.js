0acd876787cbbeb546efede06458b75aace09421
201flaviosilva <flavioandre.2001.9.7@gmail.com>
2022-07-06 11:14:03 +0000

    Add: factorial

---
+++ src/factorial.js
@@ -0,0 +1,19 @@
+/**
+ * The factorial of a non-negative​ number is computed as the product of all integers between 1 and the number itself
+ * 
+ * @see {@link https://en.wikipedia.org/wiki/Factorial}
+ * 
+ * @example factorial(0); // 0
+ * @example factorial(1); // 1
+ * @example factorial(1.2); // 0.23999999999999994
+ * @example factorial(4); // 24
+ * @example factorial(6); // 720
+ * @example factorial(10); // 3628800
+ * 
+ * @param {number} number - the number to factoring
+ * @returns {number} the result
+ */
+export function factorial(number) {
+	if (number <= 1) return number;
+	return number * factorial(number - 1);
+}
