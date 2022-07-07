82b391a59c2ec709cbf42c4945d277263d858613
201flaviosilva <flavioandre.2001.9.7@gmail.com>
2022-07-07 10:32:04 +0000

    add: Array Choice function #38

---
+++ src/arrayChoice.js
@@ -0,0 +1,16 @@
+import { randomInt } from "./randomInt";
+
+/**
+ * Returns a random item from the array
+ * 
+ * @see {@link https://www.w3schools.com/python/ref_random_choice.asp}
+ * 
+ * @example arrayChoice(["A", "B", "C"]); // "B"
+ * @example arrayChoice([10, 5, 123]); // 10
+ * 
+ * @param {any[]} array - the array to select a random item
+ * @returns {any}
+ */
+export function arrayChoice(array) {
+	return array[randomInt(array.length - 1)];
+}
